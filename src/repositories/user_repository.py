2c1bf0adb88718135b3c12ad2631ee9ebc8b4544
vfaddey <vfaddey@yandex.ru>
2024-12-15 16:32:40 +0300

    user_repository

--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -4,21 +4,27 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from uuid import UUID
 
 from src.models.user import User
+from abc import ABC, abstractmethod
 
 
-class UserRepository:
+class UserRepository(ABC):
+    @abstractmethod
     async def create(self, user):
         raise NotImplementedError
 
+    @abstractmethod
     async def get(self, user_id: int | UUID):
         raise NotImplementedError
 
+    @abstractmethod
     async def get_by_email(self, email: str):
         raise NotImplementedError
 
+    @abstractmethod
     async def update(self, user_id: int | UUID, data):
         raise NotImplementedError
 
+    @abstractmethod
     async def delete(self, user_id: int | UUID):
         raise NotImplementedError
 
@@ -27,7 +33,7 @@ class SqlaUserRepository(UserRepository):
     def __init__(self, session: AsyncSession):
         self.__session = session
 
-    async def create(self, user: User):
+    async def create(self, user: User) -> User:
         try:
             self.__session.add(user)
             await self.__session.commit()
@@ -37,17 +43,17 @@ class SqlaUserRepository(UserRepository):
             await self.__session.rollback()
             raise
 
-    async def get(self, user_id: UUID):
+    async def get(self, user_id: UUID) -> User:
         stmt = select(User).where(User.id == user_id)
         result = await self.__session.execute(stmt)
         return result.unique().scalars().first()
 
-    async def get_by_email(self, email: str):
+    async def get_by_email(self, email: str) -> User:
         stmt = select(User).where(User.email == email)
         result = await self.__session.execute(stmt)
         return result.unique().scalars().first()
 
-    async def update(self, user_id: UUID, user: User):
+    async def update(self, user_id: UUID, user: User) -> User:
         try:
             self.__session.add(user)
             await self.__session.commit()
@@ -57,7 +63,7 @@ class SqlaUserRepository(UserRepository):
             await self.__session.rollback()
             raise
 
-    async def delete(self, user_id: UUID):
+    async def delete(self, user_id: UUID) -> User:
         stmt = delete(User).where(User.id == user_id).returning(User)
         try:
             result = await self.__session.execute(stmt)
