80dd140c083acf6daf6fa089b1c2440edbf7ce22
vfaddey <vfaddey@yandex.ru>
2024-12-15 16:34:35 +0300

    new dependencies

--- src/api/deps.py
+++ src/api/deps.py
@@ -5,10 +5,14 @@ from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
+from src.adapters.producers.factory import ProducerFactory
+from src.adapters.producers.rabbitmq_producer import RabbitMQProducer
+from src.core.config import settings
 from src.db.database import AsyncSessionFactory
 from src.exceptions.user import AuthorizationException
 from src.repositories.user_repository import SqlaUserRepository
 from src.schemas.user import UserOut
+from src.services.email_service import EmailService
 from src.services.user_service import UserService
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/auth/login')
@@ -25,10 +29,13 @@ async def get_session() -> AsyncSession:
         finally:
             await session.close()
 
-
 async def get_user_service(session: AsyncSession = Depends(get_session)):
     repository = SqlaUserRepository(session)
-    return UserService(repository)
+    email_service = get_email_service()
+    return UserService(repository, email_service)
+
+def get_email_service():
+    return EmailService(producer_factory)
 
 async def get_current_user(token: str = Depends(oauth2_scheme),
                            user_service: UserService = Depends(get_user_service)) -> UserOut:
@@ -51,3 +58,7 @@ async def get_current_admin(token: str = Depends(oauth2_scheme),
     if not user.is_admin:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not enough privileges")
     return user
+
+producer_factory = ProducerFactory(RabbitMQProducer,
+                                   rabbitmq_url=settings.RABBITMQ_URL,
+                                   queue_name=settings.RABBITMQ_QUEUE)
