412671ede2f5409758fe107808bcc8cd6179e8eb
vfaddey <vfaddey@yandex.ru>
2024-12-15 16:34:04 +0300

    config update

--- src/core/config.py
+++ src/core/config.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+from pydantic import UUID4
 from pydantic_settings import BaseSettings
 from pathlib import Path
 
@@ -12,11 +13,18 @@ class Settings(BaseSettings):
     DB_PASSWORD: str
     DB_DATABASE: str
     DB_DRIVER: str = 'asyncpg'
+
     JWT_PRIVATE_KEY_PATH: Path
     JWT_PUBLIC_KEY_PATH: Path
     JWT_ALGORITHM: str = "RS256"
+
     ACCESS_TOKEN_LIFETIME: int = 60 # minutes
     REFRESH_TOKEN_LIFETIME: int = 30 # days
+    CONFIRMATION_TOKEN_LIFETIME: int = 1 # day
+
+    #rabbit
+    RABBITMQ_URL: str
+    RABBITMQ_QUEUE: str
 
     __private_key = None
     __public_key = None
@@ -46,6 +54,14 @@ class Settings(BaseSettings):
         return self.__public_key
 
 
+class Templates(BaseSettings):
+    CONFIRMATION_EMAIL_TEMPLATE: UUID4
+    RESET_PASSWORD_EMAIL_TEMPLATE: UUID4
+
+    class Config:
+        env_file = ".env.templates"
+
 settings = Settings()
+email_settings = Templates()
 
 
